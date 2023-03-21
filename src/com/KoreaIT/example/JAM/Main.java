202828e3d16f3451948648b1c2256dc24c1000b8
lifenell0411 <lifenell0411@gmail.com>
2023-03-21 13:39:39 +0900

    게시물 작성시 DB에 INSERT

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,54 +1,106 @@
 package com.KoreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	static List<Article> articles = new ArrayList<>();
-
 	public static void main(String[] args) {
-
+		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("===프로그램 시작===");
-		int lastId = 0;
+
+		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
+
 		while (true) {
 			System.out.print("명령어 ) ");
-			String command = sc.nextLine().trim();
+			String cmd = sc.nextLine().trim();
 
-			if (command.equals("article write")) {
+			if (cmd.equals("exit")) {
+				break;
+			}
+
+			if (cmd.equals("article write")) {
 				System.out.println("==게시물 작성==");
-				int id = lastId + 1;
-				System.out.print("제목 : ");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
 				String title = sc.nextLine();
-				System.out.print("내용 : ");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
-				System.out.println(id + "번 글이 생성되었습니다.");
-				lastId++;
 
-			} else if (command.equals("article list")) {
+				System.out.println(id + "번 글이 생성되었습니다");
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW(),";
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body` = '" + body + "';";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRow = pstmt.executeUpdate();
+
+					System.out.println("affectedRow : " + affectedRow);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+				lastArticleId++;
+			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
+
 				if (articles.size() == 0) {
-					System.out.println("존재하는 게시물이 없습니다.");
+					System.out.println("게시글이 없습니다");
 					continue;
 				}
-				System.out.println("번호  /  제목");
 
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				System.out.println("번호   /   제목");
 
-					System.out.println(article.id + "  /  " + article.title);
-
+				for (Article article : articles) {
+					System.out.printf("%d   /   %s\n", article.id, article.title);
 				}
-			} else if (command.equals("exit")) {
-				break;
 			}
+
 		}
-		System.out.println("===프로그램 종료===");
+
+		System.out.println("==프로그램 종료==");
 		sc.close();
 	}
-
 }
